1763c5f1580925455d92cfa54183a86dac468bdf
Sean B <strawhorse@gmail.com>
2024-06-03 16:03:38 +0100

    Created new template for site to gradually reduce issues in setup

---
+++ src/main/java/com/cct_site/cct_site/CecSiteApplication.java
@@ -0,0 +1,13 @@
+package com.cct_site.cct_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CecSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CecSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cct_site/cct_site/CecSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cct_site.cct_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CecSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
