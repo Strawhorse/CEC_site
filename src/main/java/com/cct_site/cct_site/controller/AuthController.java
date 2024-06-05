9e14ba2e90eaa640b2925afd3a064e919d86a5c3
Sean B <strawhorse@gmail.com>
2024-06-05 15:26:20 +0100

    Some tidying up on HTML templates

---
+++ src/main/java/com/cct_site/cct_site/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.cct_site.cct_site.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+//    controller which is scanned by Spring on incoming requests
+
+
+//    handler for homepage requests; maps a HTTP GET request to the /index path
+//    When someone makes a GET request to this URL, the home() method is run
+//    method returns the String "index", the name of the view template
+
+    @GetMapping("/index")
+    public String home(){
+        return "index";
+    }
+
+
+
+}
