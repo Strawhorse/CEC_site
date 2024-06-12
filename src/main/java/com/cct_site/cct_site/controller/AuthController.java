29c1cb8cf50fd2ed66f91ecf0d91b19811424a74
Sean B <strawhorse@gmail.com>
2024-06-12 16:41:21 +0100

    Updating registration controller and updating Thymeleaf template

--- src/main/java/com/cct_site/cct_site/controller/AuthController.java
+++ src/main/java/com/cct_site/cct_site/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.cct_site.cct_site.controller;
 
 
+import com.cct_site.cct_site.dto.TeacherDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -19,9 +21,17 @@ public class AuthController {
         return "account";
     }
 
+
+    // handler method to handle teacher registration form request
+//    unsure whether this should be GET or POST
+
     @GetMapping("/register")
-    public String register() {
-        return "register"; // Corresponds to register.html in the templates directory
+    public String showRegistrationForm(Model model) {
+
+        // create model object to store form data
+        TeacherDTO teacher = new TeacherDTO();
+        model.addAttribute("teacher", teacher);
+        return "register";
     }
 
 
