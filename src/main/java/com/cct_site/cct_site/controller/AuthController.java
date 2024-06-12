0463820ceb12749d49f7402c54e16bc95a12b004
Sean B <strawhorse@gmail.com>
2024-06-12 16:33:56 +0100

    Some final dependency issues resolved

--- src/main/java/com/cct_site/cct_site/controller/AuthController.java
+++ src/main/java/com/cct_site/cct_site/controller/AuthController.java
@@ -1,10 +1,10 @@
 package com.cct_site.cct_site.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+
 
 @Controller
 public class AuthController {
