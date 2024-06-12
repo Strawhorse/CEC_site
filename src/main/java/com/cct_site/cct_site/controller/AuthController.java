d639ec0037e9fae37123c2ac1a19d4718eedac8e
Sean B <strawhorse@gmail.com>
2024-06-12 14:06:37 +0100

    Fixing dependencies, reviewing possible issues with controller class

--- src/main/java/com/cct_site/cct_site/controller/AuthController.java
+++ src/main/java/com/cct_site/cct_site/controller/AuthController.java
@@ -1,11 +1,13 @@
 package com.cct_site.cct_site.controller;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class AuthController {
-
 
     @GetMapping("/index")
     public String home(){
